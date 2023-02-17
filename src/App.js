78ce145d45fcfc534e414490205b360d8e91d53b
Badass Nemesis <harshitanant29@gmail.com>
2023-02-18 00:25:45 +0530

    :)

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Transactions from './components/Transactions';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './components/Navbar';
@@ -14,14 +14,16 @@ function App() {
   return (
     <div className="App">
       <Navbar />
-      <Routes>
-        <Route exact path='/' element={<Home />} />
-        <Route path='/Transactions' element={<Transactions />} />
-        <Route path='/Logout' element={<Logout />} />
-        <Route path='/Category' element={<Category />} />
-        <Route path='/Login' element={<Login />} />
-        <Route path='/Register' element={<Register />} />
-      </Routes>
+      <BrowserRouter basename='/Personal-Finance-Project-Frontend'>
+        <Routes>
+          <Route exact path='/' element={<Home />} />
+          <Route path='/Transactions' element={<Transactions />} />
+          <Route path='/Logout' element={<Logout />} />
+          <Route path='/Category' element={<Category />} />
+          <Route path='/Login' element={<Login />} />
+          <Route path='/Register' element={<Register />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
