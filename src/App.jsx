0bdfef24183c09788d1504ca231b4755b8381e8c
Badass Nemesis <harshitanant29@gmail.com>
2023-02-17 23:02:42 +0530

    :)

--- src/App.jsx
+++ src/App.jsx
@@ -12,17 +12,16 @@ import Register from './components/register'
 
 function App() {
   return (
-
     <div className="App">
       <Navbar />
       <BrowserRouter>
         <Routes>
-          <Route exact path='/Home' element={<Home />} />
-          <Route path='/Transactions' element={<Transactions />} />
-          <Route path='/Logout' element={<Logout />} />
-          <Route path='/Category' element={<Category />} />
-          <Route path='/Login' element={<Login />} />
-          <Route path='/register' element={<Register />} />
+          <Route exact path='/Personal-Finance-Project-Frontend/' element={<Home />} />
+          <Route path='/Personal-Finance-Project-Frontend/Transactions' element={<Transactions />} />
+          <Route path='/Personal-Finance-Project-Frontend/Logout' element={<Logout />} />
+          <Route path='/Personal-Finance-Project-Frontend/Category' element={<Category />} />
+          <Route path='/Personal-Finance-Project-Frontend/Login' element={<Login />} />
+          <Route path='/Personal-Finance-Project-Frontend/register' element={<Register />} />
         </Routes>
       </BrowserRouter>
     </div>
